24af21e33778e29f95fd36dd41a9b1e6a9af8fb9
roynaldi19 <roynaldi19@gmail.com>
2021-02-19 20:22:10 +0700

    Initial commit

---
+++ app/src/main/java/com/roynaldi19/bfaa4_01readwritefile/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.roynaldi19.bfaa4_01readwritefile
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.util.*
+import kotlin.collections.ArrayList
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_new.setOnClickListener(this)
+        button_open.setOnClickListener(this)
+        button_save.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_new -> newFile()
+            R.id.button_open -> showList()
+            R.id.button_save -> saveFile()
+
+        }
+
+    }
+
+    private fun newFile() {
+        edit_title.setText("")
+        edit_file.setText("")
+        Toast.makeText(this, "Clearing file", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showList() {
+        val arrayList = ArrayList<String>()
+        val path: File = filesDir
+        Collections.addAll(arrayList, *path.list() as Array<String>)
+        val items = arrayList.toTypedArray<CharSequence>()
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Pilih File yang di inginkan")
+        builder.setItems(items) { dialog, item -> loadData(items[item].toString()) }
+        val alert = builder.create()
+        alert.show()
+
+    }
+
+    private fun loadData(title: String) {
+        val fileModel = FileHelper.readFromFile(this, title)
+        edit_title.setText(fileModel.filename)
+        edit_file.setText(fileModel.data)
+        Toast.makeText(this, "Loading " + fileModel.filename + " data", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun saveFile() {
+        when {
+            edit_title.text.toString().isEmpty() -> Toast.makeText(
+                this,
+                "Title harus diisi terlebih dahulu",
+                Toast.LENGTH_SHORT
+            ).show()
+            edit_file.text.toString().isEmpty() -> Toast.makeText(
+                this,
+                "Konten harus diisi terlebih dahulu",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            else -> {
+                val title = edit_title.text.toString()
+                val text = edit_file.text.toString()
+                val fileModel = FileModel()
+                fileModel.filename = title
+                fileModel.data = text
+                FileHelper.writeToFile(fileModel, this)
+                Toast.makeText(this, "Saving " + fileModel.filename + " file", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+}
